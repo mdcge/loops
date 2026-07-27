49b0959b7d4a6dfd6f889b1c190732d9205d7b57
Max Generowicz <mdcg-github@my-post-office.net>
2026-07-27 10:40:20 +0000

    Overload basic operators for Vector

--- cpu/main.cc
+++ cpu/main.cc
@@ -6,9 +6,7 @@ int main() {
     Vector v1(1.0, 2.0, 3.0);
     Vector v2(3.0, -4.0, 0.0);
 
-    std::cout << "Magnitude: " << v2.mag() << std::endl;
-
-    std::cout << "Normalised: (" << v2.norm().x << ", " << v2.norm().y << ", " << v2.norm().z << ")" << std::endl;
+    std::cout << "Multiply: (" << (v1*3.0).x << ", " << (3.0*v1).y << ", " << (-v1).z << ")" << std::endl;
     
     return 0;
 }
--- cpu/vector.hh
+++ cpu/vector.hh
@@ -14,6 +14,27 @@ struct Vector {
     Vector norm() const;
 };
 
-double dot(const Vector& v1, const Vector& v2);
+double dot(const Vector&, const Vector&);
+
+// Operator overloading
+inline Vector operator+(const Vector& v1, const Vector& v2) {
+    return Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+inline Vector operator-(const Vector& v1, const Vector& v2) {
+    return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+inline Vector operator-(const Vector& v) {
+    return Vector(-v.x, -v.y, -v.z);
+}
+
+inline Vector operator*(double s, const Vector& v) {
+    return Vector(s * v.x, s * v.y, s * v.z);
+}
+
+inline Vector operator*(const Vector& v, double s) {
+    return Vector(s * v.x, s * v.y, s * v.z);
+}
 
 #endif // VECTOR_H_
