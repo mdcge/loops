008c13970d5fba88310fbfe1e856d224a194e6e0
Max Generowicz <mdcg-github@my-post-office.net>
2026-07-27 11:03:19 +0000

    Add timing code

--- cpu/main.cc
+++ cpu/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "vector.hh"
 #include "photon.hh"
@@ -10,9 +11,18 @@ int main() {
     Photon photon(pos, dir, 0.0);
 
     for (int i=0; i<5; i++) {
+    // Start timer
+    auto start = std::chrono::high_resolution_clock::now();
+
         std::cout << "Position: (" << photon.r.x << ", " << photon.r.y << ", " << photon.r.z << ") at time " << photon.t << std::endl;
         photon.propagate(i);
     }
+
+    // Stop timer
+    auto end = std::chrono::high_resolution_clock::now();
+    
+    std::chrono::duration<double, std::milli> duration = end - start;
+    std::cout << "Elapsed time: " << duration.count() << " ms\n";
     
     return 0;
 }
