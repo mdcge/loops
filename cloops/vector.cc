c539fe136282402ac380f4e6037a22e1db89ee0c
Max Generowicz <mdcg-github@my-post-office.net>
2026-08-05 14:58:22 +0000

    Add Vector2D struct
    Will be used for fibre intersections, which can be solved in 2D.

--- cloops/vector.cc
+++ cloops/vector.cc
@@ -1,5 +1,6 @@
 #include "vector.hh"
 
+// 3D vector
 double Vector::mag() const {
     return std::sqrt(x * x + y * y + z * z);
 }
@@ -13,4 +14,20 @@ Vector Vector::norm() const {
 
 double dot(const Vector& v1, const Vector& v2) {
     return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+// 2D vector
+double Vector2D::mag() const {
+    return std::sqrt(x * x + y * y);
+}
+
+Vector2D Vector2D::norm() const {
+    double m = mag();
+    if (m == 0.0) { return Vector2D(0.0, 0.0); }
+    double mag_inv = 1.0 / m;
+    return Vector2D(x * mag_inv, y * mag_inv);
+}
+
+double dot(const Vector2D& v1, const Vector2D& v2) {
+    return v1.x * v2.x + v1.y * v2.y;
 }
--- cloops/vector.hh
+++ cloops/vector.hh
@@ -3,6 +3,7 @@
 
 #include <cmath>
 
+// 3D vector
 struct Vector {
     double x;
     double y;
@@ -51,6 +52,58 @@ inline Vector operator*(const Vector& v, double s) {
 
 inline Vector& operator*=(Vector& v, double s) {
     v.x *= s; v.y *= s; v.z *= s;
+    return v;
+}
+
+
+// 2D vector
+struct Vector2D {
+    double x;
+    double y;
+
+    Vector2D() : x(0.0), y(0.0) {}
+    Vector2D(double x, double y) : x(x), y(y) {}
+
+    double mag() const;
+    Vector2D norm() const;
+};
+
+double dot(const Vector2D&, const Vector2D&);
+
+
+// Operator overloading
+inline Vector2D operator+(const Vector2D& v1, const Vector2D& v2) {
+    return Vector2D(v1.x + v2.x, v1.y + v2.y);
+}
+
+inline Vector2D& operator+=(Vector2D& v1, const Vector2D& v2) {
+    v1.x += v2.x; v1.y += v2.y;
+    return v1;
+}
+
+inline Vector2D operator-(const Vector2D& v1, const Vector2D& v2) {
+    return Vector2D(v1.x - v2.x, v1.y - v2.y);
+}
+
+inline Vector2D& operator-=(Vector2D& v1, const Vector2D& v2) {
+    v1.x -= v2.x; v1.y -= v2.y;
+    return v1;
+}
+
+inline Vector2D operator-(const Vector2D& v) {
+    return Vector2D(-v.x, -v.y);
+}
+
+inline Vector2D operator*(double s, const Vector2D& v) {
+    return Vector2D(s * v.x, s * v.y);
+}
+
+inline Vector2D operator*(const Vector2D& v, double s) {
+    return Vector2D(s * v.x, s * v.y);
+}
+
+inline Vector2D& operator*=(Vector2D& v, double s) {
+    v.x *= s; v.y *= s;
     return v;
 }
 
